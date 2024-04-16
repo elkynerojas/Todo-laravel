1de1f382fa6e75177a6064d0b97646603a7c0a0e
Elkin Rojas <elkinrojas@MacBook-Pro.local>
2024-04-15 22:50:23 -0500

    Configuracion inicial

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,27 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+defineProps({ tasks: Array })
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Listado de Tareas</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <ul>
+                        <li v-for="task in tasks">
+                            {{ task.name }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
